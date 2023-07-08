1525c93d68a5f535f40ba7e398b8cefa828f4a55
Mounir Darwish <mounirdarwesh@gmail.com>
2023-07-08 12:44:37 +0200

    add Spring boot Application

---
+++ src/test/java/com/db/autocomplete/autocomplete/station/AutocompleteStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.autocomplete.autocomplete.station;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocompleteStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
